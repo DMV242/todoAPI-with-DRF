286884c65e46029b5dc69a13d60db426918677ae
Emmanueli David Valle Mvoula-Moukouyou <108657514+DMV242@users.noreply.github.com>
2024-08-29 15:50:51 +0200

    Create buid.sh

---
+++ app/buid.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Mettre à jour la liste des paquets
+sudo apt-get update
+
+# Installer les dépendances requises
+sudo apt-get install -y build-essential libsqlite3-dev libgeos-dev \
+    libproj-dev libxml2-dev libjson-c-dev libcurl4-gnutls-dev \
+    libpng-dev libtiff-dev libjpeg-dev
+
+# Installer GDAL
+sudo apt-get install -y gdal-bin libgdal-dev python3-gdal
+
+# Vérification de l'installation
+echo "GDAL version:"
+gdalinfo --version
+
+# Installation des dépendances Python
+pip install -r ../requirements.txt
+
+echo "Installation terminée avec succès."
