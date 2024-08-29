4fca00a5e3d945478964ce8e03a6a1c060bcac8d
Emmanueli David Valle Mvoula-Moukouyou <108657514+DMV242@users.noreply.github.com>
2024-08-29 15:56:05 +0200

    Update buid.sh

--- app/buid.sh
+++ app/buid.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Mettre à jour la liste des paquets
-sudo apt-get update
+apt-get update
 
 # Installer les dépendances requises
-sudo apt-get install -y build-essential libsqlite3-dev libgeos-dev \
+apt-get install -y build-essential libsqlite3-dev libgeos-dev \
     libproj-dev libxml2-dev libjson-c-dev libcurl4-gnutls-dev \
     libpng-dev libtiff-dev libjpeg-dev
 
 # Installer GDAL
-sudo apt-get install -y gdal-bin libgdal-dev python3-gdal
+sapt-get install -y gdal-bin libgdal-dev python3-gdal
 
 # Vérification de l'installation
 echo "GDAL version:"
