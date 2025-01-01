f8f986762e504f119b63bcb19d19815fb98bb686
David Mvoula <108657514+DMV242@users.noreply.github.com>
2025-01-01 15:50:47 +0100

    Update settings.py

--- app/todoAPI/settings.py
+++ app/todoAPI/settings.py
@@ -78,24 +78,14 @@ WSGI_APPLICATION = 'todoAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "todoDB",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "database-2.cjkyq680kkpa.us-east-1.rds.amazonaws.com",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
